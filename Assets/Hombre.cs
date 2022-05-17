69a9971463cc2fa8639c42e4ba075e9af2caf320
MatFreitas <matheusfs2@al.insper.edu.br>
2022-05-17 18:00:10 -0300

    collider

--- Assets/Hombre.cs
+++ Assets/Hombre.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Hombre : MonoBehaviour
 {
+    public int health = 200;
+    public int previousHealth;
+    public bool hit = false;
+    public Text Texto;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +23,31 @@ public class Hombre : MonoBehaviour
 
     void OnTriggerEnter (Collider collision)
  {
+    // Debug.Log(collision.gameObject);
+    // Debug.Log(this.gameObject.tag);
     
-     if (collision.gameObject.tag == "ChupacabraArm")
-    {
-        Debug.Log(collision.gameObject.tag);
+    if (collision.gameObject.tag == "BodyCollider" && hit)
+    {   
+        StartCoroutine(WaitPunch());
+        health -= 20;
+        Debug.Log($"vida: {health}");
+        Texto.text=$"vida: {health}";
     }
     if (collision.gameObject.tag == "Gloves")
     {
+        StartCoroutine(WaitDefense());
+        hit = false;
         Debug.Log("Luva de pedreiro");
     }
     
+ }
+
+ IEnumerator WaitPunch() {
+    yield return new WaitForSeconds(1);
+ }
+
+ IEnumerator WaitDefense() {
+    yield return new WaitForSeconds(1);
+    hit = true;
  }
 }
